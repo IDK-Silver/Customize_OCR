6288644a927357e33132fdba44bf82f218c9437b
IDK-Silver <60951976+IDK-Silver@users.noreply.github.com>
2022-05-24 15:14:28 +0800

    init

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,6 @@
-#include <QtWidgets/QApplication>
-#include <src/widget/mainwinodw/mainwindow.h>
+#include <iostream>
 
-
-int main(int argc, char* argv[])
+int main()
 {
-    APP_Setting();
-    QApplication app(argc, argv);
-    MainWindow window;
-    window.show();
-    return QApplication::exec();
+    std::cout << "Test" << std::endl;
 }
