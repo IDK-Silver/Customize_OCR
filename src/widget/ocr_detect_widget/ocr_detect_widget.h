76e0f6a106a258c99e90d3ff74a5c15c06329d6a
IDK-Silver <60951976+IDK-Silver@users.noreply.github.com>
2022-12-23 16:00:50 +0800

    change listwidget

--- src/widget/ocr_detect_widget/ocr_detect_widget.h
+++ src/widget/ocr_detect_widget/ocr_detect_widget.h
@@ -5,11 +5,9 @@
 #include <QMessageBox>
 #include <QDebug>
 #include <memory>
-#include "data_struct/ocr_data_struct.h"
-#include "src/widget/ocr_format_create_widget/data_struct/ocr_format_struct.h"
 #include <OCR/ImageOCR.h>
 #include <XlntControl/XlntControl.h>
-#include "widget_item/ocr_data_widget.h"
+#include <src/struct/OCR_Data/OCR_Data_ListWidget.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class OCR_Detect_Widget; }
@@ -36,16 +34,9 @@ private:
     void connect_setup();
 
     std::unique_ptr<ImageOCR> ocr_processing;
-    std::vector<std::shared_ptr<OCR_Format_Data>> ocr_format_data_list;
-    std::vector<std::shared_ptr<OCR_Data_Widget>> ocr_data_widget_list;
+    std::shared_ptr<OCR_Data_List> ocr_data_list;
     QStringList wait_ocr_path_list;
     std::shared_ptr<XlntControl> x_c;
-
-
-    void add_list_widget_ocr_data(const OCR_Display_Data &data);
-
-
-
 
 private slots:
     void load_setting_file();
